d54bba048ee71ce866f64b05c807db0760311b9a
Mrutyunjaya Beura <mrutyunjaya@Mrutyunjayas-MacBook-Air.local>
2024-10-02 16:03:39 +0530

    my first commit

---
+++ helloworl.java
@@ -0,0 +1,6 @@
+public class helloworl{
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+        System.out.println("this is my first program on my new mackbook air m2");
+    }
+}
